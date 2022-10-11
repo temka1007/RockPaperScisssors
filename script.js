5ab7ffae0a5be05c4bda23d0e72e69fbf2c3d8b8
Munkhtemuulen Khashbolor <munhtemuulen.kh2002@gmail.com>
2022-10-11 09:04:51 +0800

    made getComputerChoice function

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
-console.log("testing");
+const playerSelection = prompt("Choose 'rock - paper - scissor'", "")
+
+function getComputerChoice() {
+    const randomNumber = Math.floor(Math.random()*100);
+    if (randomNumber <= 33) {
+        return "Rock";
+    } else if (randomNumber <= 66) {
+        return "Paper";
+    } else {
+        return "Scissors";
+    } 
+}
+
+console.log(getComputerChoice());
+
+
+
+function playRound(playerSelection, computerSelection) {
+
+}
+
