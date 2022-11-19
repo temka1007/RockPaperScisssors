ff20d7fc648e9d0e42bf0dba68462cdfe5e48174
Munkhtemuulen Khashbolor <munhtemuulen.kh2002@gmail.com>
2022-11-19 10:08:21 +0800

    but i have to move on

--- script.js
+++ script.js
@@ -1,3 +1,17 @@
+const buttons = document.querySelectorAll('button');
+
+const player_score_1 = document.querySelector('.score.one');
+const player_score_2 = document.querySelector('.score.two');
+const player_score_3 = document.querySelector('.score.three');
+
+const computer_score_1 = document.querySelector('.score.four');
+const computer_score_2 = document.querySelector('.score.five');
+const computer_score_3 = document.querySelector('.score.six');
+
+const computerPick = document.querySelector('.computer-pick');
+const playerPick = document.querySelector('.player-pick');
+
+
 //This function tells who wins the game.
 function playRound(playerSelection, computerSelection) {
     if (playerSelection === computerSelection) {
@@ -30,19 +44,40 @@ function playRound(playerSelection, computerSelection) {
     };                                                     
 }
 
-const buttons = document.querySelectorAll('button');
-const player_score_1 = document.querySelector('.score.one');
-const player_score_2 = document.querySelector('.score.two');
-const player_score_3 = document.querySelector('.score.three');
 
-const computer_score_1 = document.querySelector('.score.four');
-const computer_score_2 = document.querySelector('.score.five');
-const computer_score_3 = document.querySelector('.score.six');
+function chosenSelectionDisplayer (computerSelection, playerSelection){
+    if (computerSelection === 'rock') {
+        computerPick.style.cssText = "background-image: url(./images/rock.png); background-size: cover;";
+    } else if (computerSelection === 'paper') {
+        computerPick.style.cssText = "background-image: url(./images/paper.png); background-size: cover;";
+    } else if ( computerSelection === 'scissors') {
+        computerPick.style.cssText = "background-image: url(./images/scissors.png); background-size: cover;";
+    };
+    if (playerSelection === 'rock') {
+        playerPick.style.cssText = "background-image: url(./images/rock.png); background-size: cover;";
+    } else if (playerSelection === 'paper') {
+        playerPick.style.cssText = "background-image: url(./images/paper.png); background-size: cover;";
+    } else if ( playerSelection === 'scissors') {
+        playerPick.style.cssText = "background-image: url(./images/scissors.png); background-size: cover";
+    };
+}
 
-const computerPick = document.querySelector('.computer-pick');
-const playerPick = document.querySelector('.player-pick');
-
-
+function scoreCounter(player, computer){
+    if (player === 1) {
+        player_score_1.style.backgroundColor =  'yellow';
+    } else if (player === 2) {
+        player_score_2.style.backgroundColor =  'yellow';
+    } else  if (player === 3) {
+        player_score_3.style.backgroundColor =  'yellow';
+    };
+    if (computer === 1) {
+        computer_score_1.style.backgroundColor =  'yellow';
+    } else if (computer === 2) {
+        computer_score_2.style.backgroundColor =  'yellow';
+    } else if (computer === 3) {
+        computer_score_3.style.backgroundColor =  'yellow';
+    };
+}
 
 function getPlayerChoice(choice) {
     return choice.id;
@@ -69,48 +104,21 @@ function game() {
             const playerSelection = getPlayerChoice(button);
             const computerSelection = getComputerChoice();
 
-            console.log(playRound(playerSelection, computerSelection));
+            playRound(playerSelection, computerSelection);
 
-            if (player === 1) {
-                player_score_1.style.backgroundColor =  'yellow';
-            } else if (player === 2) {
-                player_score_2.style.backgroundColor =  'yellow';
-            } else  if (player === 3) {
-                player_score_3.style.backgroundColor =  'yellow';
-            };
-            if (computer === 1) {
-                computer_score_1.style.backgroundColor =  'yellow';
-            } else if (computer === 2) {
-                computer_score_2.style.backgroundColor =  'yellow';
-            } else if (computer === 3) {
-                computer_score_3.style.backgroundColor =  'yellow';
-            };
+            scoreCounter(player, computer);
 
+            chosenSelectionDisplayer(computerSelection, playerSelection);
+            
             if (computer === 3) {
                 setTimeout(function(){ alert("Game is over! Computer wins!"); }, 1000);
                 setTimeout(function(){ location.reload(); }, 3000);
             };
-            
             if (player === 3) {
                 setTimeout(function(){ alert("Game is over! Player wins!"); }, 1000);
                 setTimeout(function(){ location.reload(); }, 3000);
             };
 
-           
-            if (computerSelection === 'rock') {
-                computerPick.style.cssText = "background-image: url(./images/rock.png); background-size: cover;";
-            } else if (computerSelection === 'paper') {
-                computerPick.style.cssText = "background-image: url(./images/paper.png); background-size: cover;";
-            } else if ( computerSelection === 'scissors') {
-                computerPick.style.cssText = "background-image: url(./images/scissors.png); background-size: cover;";
-            };
-            if (playerSelection === 'rock') {
-                playerPick.style.cssText = "background-image: url(./images/rock.png); background-size: cover;";
-            } else if (playerSelection === 'paper') {
-                playerPick.style.cssText = "background-image: url(./images/paper.png); background-size: cover;";
-            } else if ( playerSelection === 'scissors') {
-                playerPick.style.cssText = "background-image: url(./images/scissors.png); background-size: cover";
-            };
 
         })
     })
